c31862668756fb3462ed81ed8e97fb65de186876
ilja <ilja.ragnar@gmail.com>
2023-01-26 17:09:26 +0100

    removed vuetify

--- src/main.ts
+++ src/main.ts
@@ -2,20 +2,10 @@ import {createApp} from 'vue'
 import './style.scss'
 import App from './App.vue'
 import router from "./router"
-import 'vuetify/styles'
-import { createVuetify } from 'vuetify'
-import * as components from 'vuetify/components'
-import * as directives from 'vuetify/directives'
-
-const vuetify = createVuetify({
-    components,
-    directives,
-});
 
 
 const app = createApp(App);
 app.use(router);
-app.use(vuetify);
 router.isReady().then(() => {
     app.mount('#app');
 });
